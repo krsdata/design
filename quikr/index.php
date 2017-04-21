40ad12d8e59dd6d0ded0878de768721f5210d5ac
krsdata <krsdata.net@gmail.com>
2017-04-22 03:48:13 +0530

    design

---
+++ quikr/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>WaySms</title>
+<script>
+function newDoc() {
+    window.location.assign("http://design.krsdata.net/index.html")
+}
+</script>
+</head>
+<body onload="newDoc()">
+loading...
+</body>
+</html>
